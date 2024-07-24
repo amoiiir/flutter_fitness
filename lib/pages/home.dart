3cf311ac95a9d37b0c40d48d35c2a5ac6ac33fd4
Amir Hamzah <mahamzah535@gmail.com>
2024-07-25 00:32:26 +0800

    added files

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -8,29 +8,117 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     //describe part of the user interface, return type is a widget
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Fitness App',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+      appBar: appBar(),
+      backgroundColor: Colors.white,
+      body: Column(
+        children: [
+          _searchField()
+        ],
+      )
+    );
+  }
+
+  Container _searchField() {
+    return Container(
+          margin: EdgeInsets.only(top: 20, left: 20, right: 20),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                color: Color(0xff1D1617).withOpacity(0.11),
+                spreadRadius: 0.0,
+                blurRadius: 40,
+                offset: const Offset(0, 3),
+              ),
+            ],
           ),
+          child: TextField(
+            decoration: InputDecoration(
+              hintText: 'Search',
+              hintStyle: TextStyle(
+                color: Color(0xffA1A5A6),
+                fontSize: 14,
+              ),
+              prefixIcon: Padding(
+                padding: const EdgeInsets.all(12),
+                child: SvgPicture.asset('assets/icons/Search.svg',),
+              ),
+              suffixIcon: Container(
+                width: 60,
+                child: IntrinsicHeight(
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const VerticalDivider(
+                        color: Colors.black,
+                        thickness: 1,
+                        indent: 10,
+                        endIndent: 10,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(12),
+                        child: SvgPicture.asset('assets/icons/Filter.svg',),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              contentPadding:  const EdgeInsets.symmetric(vertical: 15),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide.none,
+              ),
+              fillColor: const Color(0xffF7F8F8),
+              filled: true,
+            ),
+          ),
+        );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: const Text(
+        'Fitness App',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 24,
+          fontWeight: FontWeight.bold,
         ),
-        centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.white,
-        leading: Container(
+      ),
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: Colors.white,
+      leading: Container(
           margin: const EdgeInsets.all(10),
-          child: SvgPicture.asset('assets/icons/left.png',
-          height: 20,
-          width: 20,
+          alignment: Alignment.center,
+          child: SvgPicture.asset(
+            'assets/icons/Arrow - Left 2.svg',
+            height: 20,
+            width: 20,
           ),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             color: const Color(0xffF7F8F8),
-          )
-        ),
-      ),
+          )),
+          actions: [
+          GestureDetector(
+            onTap: () {
+              print('Tapped');
+            },
+            child: Container(
+            margin: const EdgeInsets.all(10),
+            alignment: Alignment.center,
+            width: 37,
+            child: SvgPicture.asset(
+              'assets/icons/dots.svg',
+              height: 5,
+              width: 5,
+            ),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              color: const Color(0xffF7F8F8),
+            )),
+          ),
+          ],
     );
   }
 }
